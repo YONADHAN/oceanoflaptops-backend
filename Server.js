8353575c3ee09b058071b177b550bd393b697930
Yonadhan <yonadhanmm0@gmail.com>
2025-08-17 11:52:53 +0530

    updated CORS for frontend

--- Server.js
+++ Server.js
@@ -1,45 +1,58 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const cron = require("node-cron");
-const env = require("dotenv").config();
-const userRoute = require("./router/userRoute");
-const adminRoute = require("./router/adminRoute");
-const connectDB = require("./config/db");
-const authRoute = require('./router/authRoute');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const cron = require('node-cron')
+const env = require('dotenv').config()
+const userRoute = require('./router/userRoute')
+const adminRoute = require('./router/adminRoute')
+const connectDB = require('./config/db')
+const authRoute = require('./router/authRoute')
 const publicRoute = require('./router/publicRoute')
-const cancelPendingOrders = require("./utils/CancelOrderExceedsTwoDaysWithoutPayments");
+const cancelPendingOrders = require('./utils/CancelOrderExceedsTwoDaysWithoutPayments')
 
-connectDB();
-//const sampleController = require('./controllers/user/sampleController')
+connectDB()
+const sampleController = require('./controllers/user/sampleController')
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = ['http://localhost:5173', 'https://oceanoflaptops.shop']
+
 app.use(
   cors({
-    //origin: "http://localhost:5173", 
-     origin: ["http://localhost:5173", "https://oceanoflaptops.shop"], 
-    credentials: true, 
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`
+        return callback(new Error(msg), false)
+      }
+      return callback(null, true)
+    },
+    credentials: true,
   })
-);
+)
 
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Run every day at midnight (00:00)
-cron.schedule("0 0 * * *", () => {
-  console.log("Running scheduled job: Cancelling pending orders");
-  cancelPendingOrders();
-});
+cron.schedule('0 0 * * *', () => {
+  console.log('Running scheduled job: Cancelling pending orders')
+  cancelPendingOrders()
+})
 
-
-app.use("/api/", userRoute);
-app.use("/api/admin", adminRoute);
-app.use("/api/auth",authRoute);
-app.use("/api/public",publicRoute);
+app.use('/api/', userRoute)
+app.use('/api/admin', adminRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/public', publicRoute)
 
 app.listen(3000, () => {
-  console.log("Server started on port number 3000");
-});
+  console.log('Server started on port number 3000')
+})
